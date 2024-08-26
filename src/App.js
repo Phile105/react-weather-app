3ba563a9b9dc8603a7709cf0a8546cf2fcd87130
Phile105 <163855550+Phile105@users.noreply.github.com>
2024-08-26 14:26:23 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-
-      <footer>
-        This project was coded by Nkosingiphile Patience Mbhele and is{" "}
-        <a href="https://github.com/Phile105/react-weather-app" target="-blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <footer>
+          This project was coded by Nkosingiphile Patience Mbhele and is{" "}
+          <a
+            href="https://github.com/Phile105/react-weather-app"
+            target="-blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
