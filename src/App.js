6aed52430841d0d2c65428218566183fbde2b813
Phile105 <163855550+Phile105@users.noreply.github.com>
2024-09-04 19:23:11 +0200

    Refactored code and fixed warnings

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ export default function App() {
           <a
             href="https://github.com/Phile105/react-weather-app"
             target="-blank"
+            rel="noopener noreferrer"
           >
             open-sourced on GitHub
           </a>{" "}
